21d8391472d84db34501bed28d4a9a06d83fcc8e
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-08-26 01:22:22 -0300

    feat: initialize AppDataSource

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 const PORT = 3333;
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import { AppDataSource } from "./configs/db";
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,12 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
